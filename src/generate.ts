261ba0de93e9a364a3282c11f30067095db6cac9
tequ <git@tequ.dev>
2024-01-26 21:32:16 +0900

    refactor generate script

--- src/generate.ts
+++ src/generate.ts
@@ -5,59 +5,70 @@ import { GovernanceHookDefinition } from './hook-schemas/xahau-governance'
 import { hookStateParser } from './parser'
 
 import * as fs from 'fs'
+import { Definition } from './schema'
 
 const client = new Client('wss://xahau.org')
 // const client = new Client("wss://xahau-test.net");
+const outputsDir = './outputs'
 
 const GENESIS_ACCOUNT = 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh'
 
-const Xahau_Governance = {
+type DefinitionSource = {
+  name: string
+  hook_account: string
+  hook_namespace_id: string
+  hook_definition: Definition['hook_states']
+}
+
+const Xahau_Governance: DefinitionSource = {
   name: 'xahau-governance',
   hook_account: GENESIS_ACCOUNT,
   hook_namespace_id: '0',
   hook_definition: GovernanceHookDefinition,
 }
 
-const Evernode = {
+const Evernode: DefinitionSource = {
   name: "evernode",
   hook_account: 'rBvKgF3jSZWdJcwSsmoJspoXLLDVLDp6jg',
   hook_namespace_id: '01EAF09326B4911554384121FF56FA8FECC215FDDE2EC35D9E59F2C53EC665A0',
   hook_definition: EvernodeHookDefinition,
 }
 
+const definitions = [Xahau_Governance, Evernode]
 
-const current = Evernode
-const outputsDir = './outputs'
-const definitionDir = `${outputsDir}/${current.name}`
+const generateDefinitionJson = (source: DefinitionSource, dir: string) => {
+  const j = JSON.stringify(source, null, 2);
+  fs.mkdirSync(dir, { recursive: true })
+  fs.writeFileSync(`${dir}/${source.name}-definition.json`, j);
+}
 
-const main = async () => {
-  await client.connect()
+const generateStateJson = (source: DefinitionSource, data: any, dir: string) => {
+  const json = JSON.stringify(data, (_, value) => typeof value === "bigint" ? value.toString() : value, 2);
+  fs.writeFileSync(`${dir}/${source.name}-state.json`, json);
+}
 
-  // output definition
-  const j = JSON.stringify(current, null, 2);
-
-  fs.mkdirSync(definitionDir, { recursive: true })
-
-  fs.writeFileSync(`${definitionDir}/${current.name}-definition.json`, j);
-
+const fetchStateData = async (source: DefinitionSource) => {
   const response = await client.request({
     command: 'account_namespace',
-    account: current.hook_account,
-    namespace_id: current.hook_namespace_id,
+    account: source.hook_account,
+    namespace_id: source.hook_namespace_id,
     limit: 10000,
   })
   // @ts-ignore
-  const entries = response.result.namespace_entries as HookState[]
+  return response.result.namespace_entries as HookState[]
+}
 
-  const r = entries.map((entry) => {
-    return hookStateParser(entry, current.hook_definition)
-  })
-
-  const json = JSON.stringify(r, (_, value) => typeof value === "bigint" ? value.toString() : value, 2);
-
-  // output parsed state data
-  fs.writeFileSync(`${definitionDir}/${current.name}-state.json`, json);
-
+const main = async () => {
+  await client.connect()
+  for (const current of definitions) {
+    const definitionDir = `${outputsDir}/${current.name}`
+    generateDefinitionJson(current, definitionDir)
+    const states = await fetchStateData(current)
+    const r = states.map((state) => {
+      return hookStateParser(state, current.hook_definition)
+    })
+    generateStateJson(current, r, definitionDir)
+  }
   await client.disconnect()
 }
 
