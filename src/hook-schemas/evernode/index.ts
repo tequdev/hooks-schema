79a06f349f5d160af806cb827c4fc7be5c5a0cd3
tequ <git@tequ.dev>
2024-01-26 10:31:31 +0900

    fix import

--- src/hook-schemas/evernode/index.ts
+++ src/hook-schemas/evernode/index.ts
@@ -1,9 +1,9 @@
-import { HookStateDefinition } from '../../schema'
+import { Definition } from '../../schema'
 import { all as all1 } from './common/configuration'
 import { all as all3 } from './common/repetitive-state-keys'
 import { all as all2 } from './common/singleton-keys'
 
-export const EvernodeHookDefinition: HookStateDefinition = {
+export const EvernodeHookDefinition: Definition['hook_states'] = {
   name: 'Evernode',
   description: 'Evernode',
   hook_states: [...all1, ...all2, ...all3],
