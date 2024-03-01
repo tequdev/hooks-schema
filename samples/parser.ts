83e37514918d86a30a4facf0044a5ab318513b35
tequ <git@tequ.dev>
2024-03-02 01:55:45 +0900

    refactor parser

--- samples/parser.ts
+++ samples/parser.ts
@@ -16,49 +16,35 @@ const getLengthPrefix = (buffer: Buffer) => {
   return len
 }
 
-const getByteLength = (state: Field, buffer: Buffer) => {
-  switch (state.type) {
-    case 'Array':
-      if (state.byte_length) return state.byte_length
-      if (state.length_prefix) return getLengthPrefix(buffer)
-      throw new Error('Array: length_prefix or byte_length must be defined')
-    case 'AccountID':
-      return 20
-    case 'UInt8':
-      return 1
-    case 'UInt16':
-      return 2
-    case 'UInt32':
-      return 4
-    case 'UInt64':
-      return 8
-    case 'XFL':
-      return 8
-    case 'VarString':
-      if (state.byte_length) return state.byte_length
-      if (state.length_prefix) return getLengthPrefix(buffer)
-      throw new Error('VarString: length_prefix or byte_length must be defined')
-    case 'Hash256':
-      return 32
-    default:
-      throw new Error('Invalid type')
+const getTypeByteLength = (type: Field['type']) => {
+  const typeToLength: Record<Field['type'], number | undefined> = {
+    AccountID: 20,
+    UInt8: 1,
+    UInt16: 2,
+    UInt32: 4,
+    UInt64: 8,
+    XFL: 8,
+    Hash256: 32,
+    Array: undefined,
+    VarString: undefined,
   }
+  return typeToLength[type]
 }
 
-const bufferToReadableData = (buffer: Buffer, state: Field, nullReplaceTo = '0'): string | number | bigint | Array<string | number | bigint> => {
+const getByteLength = (state: Field, buffer: Buffer) => {
+  if (state.byte_length) return state.byte_length
+  if (state.type === 'Array' && state.length_prefix) return getLengthPrefix(buffer)
+  const fixedLength = getTypeByteLength(state.type)
+  if (fixedLength) return fixedLength
+  throw new Error(`${state.type}: length_prefix, byte_length, or a valid fixed length type must be defined`)
+}
+
+const bufferToReadableData = (
+  buffer: Buffer,
+  state: Field,
+  nullReplaceTo = '0',
+): string | number | bigint | Array<string | number | bigint> => {
   switch (state.type) {
-    case 'Array': {
-      let from = 0
-      const a = state.array.map((s) => {
-        const to = from + getByteLength(s, buffer.subarray(from))
-        const buf = buffer.subarray(from, to)
-        from = to
-        return { name: s.name, value: bufferToReadableData(buf, s as Field, nullReplaceTo) as string | number | bigint }
-      })
-      if (state.delimiter) return a.map(_a => _a).join(state.delimiter)
-      // @ts-ignore
-      return a
-    }
     case 'AccountID':
       return encodeAccountID(buffer)
     case 'UInt8':
@@ -76,6 +62,18 @@ const bufferToReadableData = (buffer: Buffer, state: Field, nullReplaceTo = '0')
       return buffer.toString('utf-8').replace(/\0/g, nullReplaceTo)
     case 'Hash256':
       return buffer.toString('hex').toUpperCase()
+    case 'Array': {
+      let from = 0
+      const a = state.array.map((s: Field) => {
+        const length = getByteLength(s, buffer.subarray(from))
+        const buf = buffer.subarray(from, from + length)
+        from += length
+        return { name: s.name, value: bufferToReadableData(buf, s, nullReplaceTo) as string | number | bigint }
+      })
+      if (state.delimiter) a.map((_a) => _a.value).join(state.delimiter)
+      // @ts-ignore
+      return a
+    }
     default:
       throw new Error('Invalid type')
   }
@@ -85,245 +83,97 @@ const fieldsTotalLength = (fields: Field[], buffer: Buffer) => {
   return fields.reduce((total, cur) => total + getByteLength(cur, buffer.subarray(total)), 0)
 }
 
-const parseBuffer = (buffer: Buffer, fields: Field[], nullReplaceTo = ''): ReadableData[] => {
-  let lastIndex = 0;
-  return fields.map((field) => {
-    const length = getByteLength(field, buffer.subarray(lastIndex));
-    const currentBuffer = buffer.subarray(lastIndex, lastIndex + length);
-    lastIndex += length;
-    const value = bufferToReadableData(currentBuffer, field, nullReplaceTo);
-    return { name: field.name, value };
-  }).filter(field => field.value !== undefined);
-};
-
-
 type ReadableData = {
   [key: string]: string | number | bigint | Array<string | number | bigint>
+}
+
+const parseBuffer = (buffer: Buffer, definitions: Field[]) => {
+  let lastIndex = 0
+  let lastBuffer = buffer
+  const readableArr: ReadableData[] = []
+
+  const check = definitions.every((d) => {
+    const currentIndex = getByteLength(d, lastBuffer)
+    const currentBuffer = buffer.subarray(lastIndex, lastIndex + currentIndex)
+    lastIndex += currentIndex
+    lastBuffer = currentBuffer
+    const readableForComp = bufferToReadableData(currentBuffer, d, '0')
+    const readable = bufferToReadableData(currentBuffer, d, '')
+
+    if (d.pattern && !new RegExp(d.pattern).test(readableForComp.toString())) return false
+    if (!d.exclude) {
+      readableArr.push({
+        name: d.name,
+        value: readable,
+      })
+    }
+    return true
+  })
+
+  return check ? readableArr : undefined
+}
+
+type KeyValueDefs = HookStateDefinition['hook_states'] | TxnParameterDefinition['txn_parameters'] | HookParameterDefinition['hook_parameters']
+
+const parseKeyValue = (
+  keyBuffer: Buffer,
+  dataBuffer: Buffer,
+  definition: any,
+  keyDefinition: string,
+  dataDefinition: string,
+) => {
+  if (keyBuffer.length !== fieldsTotalLength(definition[keyDefinition], keyBuffer)) return undefined
+  if (dataBuffer.length !== fieldsTotalLength(definition[dataDefinition], dataBuffer)) return undefined
+
+  const keyArr = parseBuffer(keyBuffer, definition[keyDefinition])
+  const dataArr = parseBuffer(dataBuffer, definition[dataDefinition])
+
+  if (!keyArr || !dataArr) return undefined
+
+  return { name: definition.name, key: keyArr, data: dataArr }
+}
+
+type SingleDataDefs = InvokeBlobDefinition['invoke_blobs']
+
+const parseSingleData = (buffer: Buffer, definition: any) => {
+  const readableArr = parseBuffer(buffer, definition.value)
+  if (readableArr) return { name: definition.name, values: readableArr }
+  return undefined
+}
+
+
+const wrapper = <T extends KeyValueDefs | SingleDataDefs>(definition: T, fn: (def: T[number]) => any) => {
+  return definition
+    .map(fn)
+    .filter((d): d is NonNullable<typeof d> => typeof d !== 'undefined')
+    .find((_) => true)
 }
 
 export const hookStateParser = (value: HookState, definition: HookStateDefinition) => {
   const key = Buffer.from(value.HookStateKey, 'hex')
   const data = Buffer.from(value.HookStateData, 'hex')
-  // console.log(value)
-  const state = definition.hook_states
-    .map((state) => {
-      // console.log(data.length, fieldsTotalLength(state))
-      if (data.length !== fieldsTotalLength(state.hookstate_data, data)) return undefined
 
-      let lastKeyIndex = 0
-      let lastBuffer = key
-      const keyArr = parseBuffer(key, state.hookstate_key)
-      // console.log(state.hookstate_key)
-      const checkKey = state.hookstate_key.every((k) => {
-        // Retrieve info matching HookStateKey and length with definition
-        const currentKeyIndex = getByteLength(k, lastBuffer)
-        const currentBuffer = key.subarray(lastKeyIndex, lastKeyIndex + currentKeyIndex)
-        lastKeyIndex += currentKeyIndex
-        lastBuffer = currentBuffer
-        const readableForComp = bufferToReadableData(currentBuffer, k, "0")
-        const readable = bufferToReadableData(currentBuffer, k, "")
-        // console.log(lastKeyIndex, lastKeyIndex + currentKeyIndex, readable)
-        // console.log(k.pattern, readable.toString(), !new RegExp(k.pattern!).test(readable.toString()))
-        if (k.pattern && !new RegExp(k.pattern).test(readableForComp.toString())) return false
-        if (k.exclude === true) return true
-        keyArr.push({
-          name: k.name,
-          value: readable,
-        })
-        return true
-      })
-      // console.log({ checkKey })
-      if (checkKey === false) return undefined
-      let lastDataIndex = 0
-      lastBuffer = data
-      const dataArr: ReadableData[] = []
-      const checkData = state.hookstate_data.every((d) => {
-        // Retrieve info matching HookStateData and length with definition
-        const currentDataIndex = getByteLength(d, lastBuffer)
-        const currentBuffer = data.subarray(lastDataIndex, lastDataIndex + currentDataIndex)
-        lastDataIndex += currentDataIndex
-        lastBuffer = currentBuffer
-        const readableForComp = bufferToReadableData(currentBuffer, d, "0")
-        const readable = bufferToReadableData(currentBuffer, d, "")
-        // console.log(lastDataIndex - currentDataIndex, lastDataIndex, readable, currentBuffer)
-        // console.log(d.pattern, readable.toString(), !new RegExp(d.pattern!).test(readable.toString()))
-
-        if (d.pattern && !new RegExp(d.pattern).test(readableForComp.toString())) return false
-        if (d.exclude === true) return true
-        dataArr.push({
-          name: d.name,
-          value: readable,
-        })
-        return true
-      })
-
-      return checkData ? { name: state.name, key: keyArr, data: dataArr } : undefined
-    })
-    .filter((d): d is NonNullable<typeof d> => typeof d !== 'undefined')
-    .find((_) => true)
-  return state
+  return wrapper(definition.hook_states, (state) => parseKeyValue(key, data, state, 'hookstate_key', 'hookstate_data'))
 }
 
 export const invokeBlobParser = (blob: string, definition: InvokeBlobDefinition) => {
-  console.log(blob)
   const data = Buffer.from(blob, 'hex')
-  return definition.invoke_blobs.map((def) => {
-    // TODO: check length
-    // if (data.length !== fieldsTotalLength(def.value, data)) return undefined
 
-    let lastKeyIndex = 0
-    let lastBuffer = data
-    const readableArr: ReadableData[] = []
-    // console.log(state.hookstate_key)
-    const checkBlob = def.value.every((d) => {
-      // Retrieve info matching HookStateKey and length with definition
-      const currentKeyIndex = getByteLength(d, lastBuffer)
-      const currentBuffer = data.subarray(lastKeyIndex, lastKeyIndex + currentKeyIndex)
-      lastKeyIndex += currentKeyIndex
-      lastBuffer = currentBuffer
-      const readableForComp = bufferToReadableData(currentBuffer, d, "0")
-      const readable = bufferToReadableData(currentBuffer, d, "")
-
-      if (d.pattern && !new RegExp(d.pattern).test(readableForComp.toString())) return false
-      if (d.exclude === true) return true
-      readableArr.push({
-        name: d.name,
-        value: readable,
-      })
-      return true
-    })
-    if (checkBlob) return { name: def.name, values: readableArr }
-  })
-    .filter((d): d is NonNullable<typeof d> => typeof d !== 'undefined')
-    .find((_) => true)
+  return wrapper(definition.invoke_blobs, (def) => parseSingleData(data, def))
 }
 
 export const txnParametersParser = (parameters: HookParameter, definition: TxnParameterDefinition) => {
   const { HookParameterName, HookParameterValue } = parameters.HookParameter
   const name = Buffer.from(HookParameterName, 'hex')
   const value = Buffer.from(HookParameterValue, 'hex')
-  // console.log(value)
-  const state = definition.txn_parameters
-    .map((state) => {
-      // console.log(data.length, fieldsTotalLength(state))
-      if (name.length !== fieldsTotalLength(state.otxnparam_key, name)) return undefined
-      if (value.length !== fieldsTotalLength(state.otxnparam_data, value)) return undefined
 
-      let lastNameIndex = 0
-      let lastBuffer = name
-      const keyArr: ReadableData[] = []
-
-      const checkKey = state.otxnparam_key.every((k) => {
-        // Retrieve info matching HookStateKey and length with definition
-        const currentNameIndex = getByteLength(k, lastBuffer)
-        const currentBuffer = name.subarray(lastNameIndex, lastNameIndex + currentNameIndex)
-        lastNameIndex += currentNameIndex
-        lastBuffer = currentBuffer
-        const readableForComp = bufferToReadableData(currentBuffer, k, "0")
-        const readable = bufferToReadableData(currentBuffer, k, "")
-        // console.log(lastNameIndex, lastNameIndex + currentNameIndex, readable)
-        // console.log(k.pattern, readable.toString(), !new RegExp(k.pattern!).test(readable.toString()))
-        if (k.pattern && !new RegExp(k.pattern).test(readableForComp.toString())) return false
-        if (k.exclude === true) return true
-        keyArr.push({
-          name: k.name,
-          value: readable,
-        })
-        return true
-      })
-      // console.log({ checkKey })
-      if (checkKey === false) return undefined
-      let lastValueIndex = 0
-      lastBuffer = value
-      const dataArr: ReadableData[] = []
-      const checkData = state.otxnparam_data.every((d) => {
-        // Retrieve info matching HookStateData and length with definition
-        const currentDataIndex = getByteLength(d, lastBuffer)
-        const currentBuffer = value.subarray(lastValueIndex, lastValueIndex + currentDataIndex)
-        lastValueIndex += currentDataIndex
-        lastBuffer = currentBuffer
-        const readableForComp = bufferToReadableData(currentBuffer, d, "0")
-        const readable = bufferToReadableData(currentBuffer, d, "")
-        // console.log(lastValueIndex - currentDataIndex, lastValueIndex, readable, currentBuffer)
-        // console.log(d.pattern, readable.toString(), !new RegExp(d.pattern!).test(readable.toString()))
-
-        if (d.pattern && !new RegExp(d.pattern).test(readableForComp.toString())) return false
-        if (d.exclude === true) return true
-        dataArr.push({
-          name: d.name,
-          value: readable,
-        })
-        return true
-      })
-
-      return checkData ? { name: state.name, key: keyArr, data: dataArr } : undefined
-    })
-    .filter((d): d is NonNullable<typeof d> => typeof d !== 'undefined')
-    .find((_) => true)
-  return state
+  return wrapper(definition.txn_parameters, (state) => parseKeyValue(name, value, state, 'otxnparam_key', 'otxnparam_data'))
 }
 
 export const hookParametersParser = (parameters: HookParameter, definition: HookParameterDefinition) => {
   const { HookParameterName, HookParameterValue } = parameters.HookParameter
   const name = Buffer.from(HookParameterName, 'hex')
   const value = Buffer.from(HookParameterValue, 'hex')
-  // console.log(value)
-  const state = definition.hook_parameters
-    .map((state) => {
-      // console.log(data.length, fieldsTotalLength(state))
-      if (name.length !== fieldsTotalLength(state.hookparam_key, name)) return undefined
-      if (value.length !== fieldsTotalLength(state.hookparam_data, value)) return undefined
 
-      let lastNameIndex = 0
-      let lastBuffer = name
-      const keyArr: ReadableData[] = []
-
-      const checkKey = state.hookparam_key.every((k) => {
-        // Retrieve info matching HookStateKey and length with definition
-        const currentNameIndex = getByteLength(k, lastBuffer)
-        const currentBuffer = name.subarray(lastNameIndex, lastNameIndex + currentNameIndex)
-        lastNameIndex += currentNameIndex
-        lastBuffer = currentBuffer
-        const readableForComp = bufferToReadableData(currentBuffer, k, "0")
-        const readable = bufferToReadableData(currentBuffer, k, "")
-        // console.log(lastNameIndex, lastNameIndex + currentNameIndex, readable)
-        // console.log(k.pattern, readable.toString(), !new RegExp(k.pattern!).test(readable.toString()))
-        if (k.pattern && !new RegExp(k.pattern).test(readableForComp.toString())) return false
-        if (k.exclude === true) return true
-        keyArr.push({
-          name: k.name,
-          value: readable,
-        })
-        return true
-      })
-      // console.log({ checkKey })
-      if (checkKey === false) return undefined
-      let lastValueIndex = 0
-      lastBuffer = value
-      const dataArr: ReadableData[] = []
-      const checkData = state.hookparam_data.every((d) => {
-        // Retrieve info matching HookStateData and length with definition
-        const currentDataIndex = getByteLength(d, lastBuffer)
-        const currentBuffer = value.subarray(lastValueIndex, lastValueIndex + currentDataIndex)
-        lastValueIndex += currentDataIndex
-        lastBuffer = currentBuffer
-        const readableForComp = bufferToReadableData(currentBuffer, d, "0")
-        const readable = bufferToReadableData(currentBuffer, d, "")
-        // console.log(lastValueIndex - currentDataIndex, lastValueIndex, readable, currentBuffer)
-        // console.log(d.pattern, readable.toString(), !new RegExp(d.pattern!).test(readable.toString()))
-
-        if (d.pattern && !new RegExp(d.pattern).test(readableForComp.toString())) return false
-        if (d.exclude === true) return true
-        dataArr.push({
-          name: d.name,
-          value: readable,
-        })
-        return true
-      })
-
-      return checkData ? { name: state.name, key: keyArr, data: dataArr } : undefined
-    })
-    .filter((d): d is NonNullable<typeof d> => typeof d !== 'undefined')
-    .find((_) => true)
-  return state
+  return wrapper(definition.hook_parameters, (state) => parseKeyValue(name, value, state, 'hookparam_key', 'hookparam_data'))
 }
