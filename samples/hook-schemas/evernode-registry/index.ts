66952bf302d125403d7e45dcf9d412c4a883357d
tequ <git@tequ.dev>
2024-03-01 23:44:13 +0900

    add evernode registry hook

---
+++ samples/hook-schemas/evernode-registry/index.ts
@@ -0,0 +1,10 @@
+import { Definition } from '../../../schema'
+import { EvernodeRegistryHookParametersDefinition } from './hook_parameters'
+
+export const EvernodeHeartbeatHookDefinition: Definition = {
+  name: 'Evernode Heartbeat',
+  description: 'Evernode Heartbeat',
+  hook_hash: 'FB7159503D2F6572B539B65994028B1E945871A5BCC7BB2ECDF63CE34ECB2945',
+  version: [],
+  hook_parameters: EvernodeRegistryHookParametersDefinition
+}
